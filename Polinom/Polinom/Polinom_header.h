78fcbda5ebd2d146760b3bf02bbe0a44d5f291fa
Кириллов Никита Дмитриевич <kirillov.n@ccam.cmc.unn.net>
2021-02-20 14:30:05 +0300

    commit 2

--- Polinom/Polinom/Polinom_header.h
+++ Polinom/Polinom/Polinom_header.h
@@ -1,1 +1,28 @@
-_#pragma once
+#pragma once
+template <class T>
+struct TLink
+{
+	T val;
+	TLink *pNext;
+};
+template <class T>
+class TList
+{
+protected:
+	TLink<T>*pFirst, *pLast, *pCurr, *pPrev, *pStop;
+	int size;
+public:
+	TList()
+	{
+		pStop = NULL;
+		pFirst = pStop;
+		pLast = pStop;
+		pCurr = pStop;
+		pPrev = pStop;
+		size = 0;
+	}
+	~TList()
+	{
+
+	}
+};
